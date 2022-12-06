9d652d05e5cad3c3665e119cac4aa989ea31fef5
david castro salazar <david.cassa@hotmail.com>
2022-12-07 00:16:22 +0100

    Deleting anucesay files

--- Source/libs/assimp/out/build/x64-Debug/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/david/OneDrive/Escritorio/Master/Grafico 3d/Engine/Engine/UPC-Videogame-Engine/Source/libs/assimp/contrib/zlib
-# Build directory: C:/Users/david/OneDrive/Escritorio/Master/Grafico 3d/Engine/Engine/UPC-Videogame-Engine/Source/libs/assimp/out/build/x64-Debug/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
